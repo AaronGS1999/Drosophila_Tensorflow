160c7e035c74d7a915de82ab0526fe54d11f7659
Aaron Gálvez Salido <92391739+AaronGS1999@users.noreply.github.com>
2022-04-25 20:05:50 +0200

    Update split_folder.py

--- DATA PREPARATION/split_folder.py
+++ DATA PREPARATION/split_folder.py
@@ -1,3 +1,3 @@
 import splitfolders
-folder = "C:/Users/aaron/Desktop/Tensorflow drosophila/divisor_fotos/dataset"
+folder = "" # Add the address of your folders, keep in mind that they must have a specific structure of the library
 splitfolders.ratio(folder, output="output", seed=1999, ratio=(.8, 0.1, 0.1)) 
