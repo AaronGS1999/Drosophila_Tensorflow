d109a49a7881247ce46caca59495a8dad7d463c4
Aaron Gálvez Salido <92391739+AaronGS1999@users.noreply.github.com>
2022-05-23 17:04:43 +0200

    Update split_folder.py

--- DATA PREPARATION/split_folder.py
+++ DATA PREPARATION/split_folder.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 import splitfolders
 folder = "" # Add the address of your folders, keep in mind that they must have a specific structure of the library
 splitfolders.ratio(folder, output="output", seed=1999, ratio=(.8, 0.1, 0.1)) 
